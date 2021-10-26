a926d0123b233fb30c3056c513bd2a0cc759fab0
Devin Mullin <devin.mullin90@gmail.com>
2021-10-26 15:46:50 -0500

    form synced with db

--- src/components/BoonForm.jsx
+++ src/components/BoonForm.jsx
@@ -1,23 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Select from "react-select";
 
 function BoonForm(){
-const options = [
-    {label: "Aphrodite",
-    options:[
-    { value: 'heartbreak-strike', label: 'Heartbreak Strike' },
-    { value: 'heartbreak-flourish', label: 'Heartbreak Flourish' },
-    { value: 'crush-shot', label: 'Crush Shot' },
-    { value: 'passion-flare', label: 'Passion Flare' }, 
-    { value: 'passion-dash', label: 'Passion Dash' },
-    { value: 'aphrodites-aid', label: 'Aphrodite\'s Aid' },
-    { value: 'dying-lament', label: 'Dying Lament' },
-    { value: 'wave-of-despair', label: 'Wave of Despair' },
-    { value: 'different-league', label: 'Different League' },
-    { value: 'life-affirmation', label: 'Life Affirmation' },
-    { value: 'empty-inside', label: 'Empty Inside' }
-  ]}];
-  
+const [boons, setBoons] = useState([])
+
+useEffect(()=> {
+    fetch('http://localhost:9292/boons')
+    .then(r=>r.json()
+    .then(d => setBoons(d)))
+}, [])
+
+const options = boons.map(boon => ({ label: boon.god_id, options:[{ value: boon.id, label: boon.boon_name }]}))
+
 return (
     <div>
         <h3>ready to pick your boons? well then...</h3>
