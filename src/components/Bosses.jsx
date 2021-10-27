2db7505da2f97289c0d0c7293dc4b021fcc7b0cf
Patrick Wu <Ptrckwu@yahoo.com>
2021-10-27 16:41:32 -0400

    big brains

--- src/components/Bosses.jsx
+++ src/components/Bosses.jsx
@@ -2,46 +2,55 @@ import { useEffect, useState } from "react"
 
 
 function Bosses(){
-const [bosses, setBosses] = useState([])
-
-    useEffect(()=>{
+const [allBosses, setAllBosses] = useState([])
+const [downedBoss, setDownedBoss] = useState({
+    first_boss_down: false,
+    second_boss_down: false,
+    third_boss_down: false,
+    hades_down_down: false
+})
+    useEffect(()=>{ 
         fetch("http://localhost:9292/bosses")
         .then(r=>r.json())
-        .then(b=>setBosses(b))
+        .then(b=>setAllBosses(b))
     }, [])
 
-    const handleClick = (e) =>{
-        alert('grats')
-        // fetch("http://localhost:9292/beaten_bosses", {
-        //     method: "POST",
-        //     headers: {
-        //       "Content-Type": "application/json",
-        //     },
-        //     body: JSON.stringify({
-        //       comment: comment,
-        //       score: score,
-        //       user_id: userId,
-        //       game_id: gameId,
-        //     }),
-        //   })
-        //     .then((r) => r.json())
+    function handleSubmit(e){
+        e.target.default
+        addBoss(downedBoss)
     }
 
+    function addBoss(downedBosses){
+        fetch("http://localhost:9292/beaten_bosses", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(downedBosses)
+        })
+        .then(r=>r.json())
+        .then(console.log)
+    }
+    const handleClick = (e) =>{
+        alert('grats')
+       setDownedBoss(!downedBoss.first_boss_down)
+    }
+    // const bossnameabossrray = bosses.map(b=> b.boss_name)
 
 
     return(
-        <div>
+        <form onSubmit={handleSubmit}>
         <h2>how far did you get???</h2>
         <ol>
         <h3>THE FURIES</h3>
-        <li><button onClick={handleClick}>{bosses[0].boss}</button></li>
-        <li><button onClick={handleClick}>{bosses[1].boss}</button></li>
-        <li><button onClick={handleClick}>{bosses[2].boss}</button></li>
+        <li><button onClick={handleClick}>{allBosses[0]?.boss_name}</button></li>
+        <li><button onClick={handleClick}>{allBosses[1]?.boss_name}</button></li>
+        <li><button onClick={handleClick}>{allBosses[2]?.boss_name}</button></li>
         </ol>
-        <button onClick={handleClick}>{bosses[3].boss}</button>
-        <button onClick={handleClick}>{bosses[4].boss}</button>
-        <button onClick={handleClick}>{bosses[5].boss}</button>
-        </div>
+        <button onClick={handleClick}>{allBosses[3]?.boss_name}</button>
+        <button onClick={handleClick}>{allBosses[4]?.boss_name}</button>
+        <button onClick={handleClick}>{allBosses[5]?.boss_name}</button>
+        </form>
     )
 
 }
